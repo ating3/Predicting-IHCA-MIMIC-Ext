47a7cab11e4a84adf4f4b61d19c8cd7f6de49445
ating3 <jtcybs@gmail.com>
2026-02-03 22:21:48 -0800

    Refactor query.py for vital data processing
    Refactor query.py to integrate preprocessing and create vital tables for patient data analysis.

--- query.py
+++ query.py
@@ -1,229 +1,94 @@
+import preprocessing_query
+import os
 import pandas as pd
-import datetime as dt
+from icdmappings import Validator
+from icdmappings import Mapper
 import polars as pl
 
+icu_dir = os.getenv("ICU_PATH")
+hosp_dir = os.getenv("HOSP_PATH")
 
-vitals_code = {
-    #"temp": 223761,
-    "HR": 220045,
-    "RR": 220210,  # Respiratory Rate
-    "SpO2": 220277,
-    "SBP": 220179,
-    "DBP": 220180,
-    "MAP": 220181,  # Mean Arterial Pressure
-}
+#TIME INDEPENDENT
+procedure_path = os.path.join(icu_dir, "procedureevents.csv")
+#has procedure codes
 
-def create_ihca_list(procedure_df, icustays_df):
-    #grabs patients with cardiac resuscitation procedure and relevant icu stay, filters for los > 1.0 and 
-    #subject_id: patient id
-    #reference: beginning of cardiac resuscitation event
-    #stay_id: relevant icu stay of ihca event
+icustays_path = os.path.join(icu_dir, "icustays.csv")
+#has ICU duration and admission type
 
-    ihca_events = procedure_df[procedure_df["itemid"] == 225466][["subject_id", "starttime"]].copy()
-    merged = ihca_events.merge(icustays_df[['subject_id', 'stay_id', 'intime', 'outtime', 'los', 'first_careunit']], on='subject_id', how='left')
-    in_stay = (merged['starttime'] >= merged['intime']) & (merged['starttime'] <= merged['outtime']) & (merged['los'] >= 1.0)
-    merged = merged[in_stay]
-    merged = merged.rename(columns={'starttime': 'referencetime'})
-    merged["referencetime"] = merged["referencetime"].dt.strftime("%Y-%m-%d %H:%M:%S")
-    merged = merged.drop_duplicates(subset=["subject_id", "stay_id", "referencetime"])
+admissions_path = os.path.join(hosp_dir, "admissions.csv")
+#has ethnicity
 
-    return merged[['subject_id', 'stay_id', 'referencetime']]
+patients_path = os.path.join(hosp_dir, "patients.csv")
+#has age(anchor_age) and sex
 
-def create_controls_list(icustays_df, ihca_list, n = 956):
-    controls = icustays_df[icustays_df['los'] >= 1.0].copy()
-    merged = controls.merge(ihca_list[['subject_id','stay_id']], on=['subject_id', 'stay_id'], how='left', indicator=True)
-    merged = merged[merged['_merge'] == 'left_only'].drop(columns='_merge')
-    merged = merged.rename(columns={'outtime': 'referencetime'})
-    merged["referencetime"] = merged["referencetime"].dt.strftime("%Y-%m-%d %H:%M:%S")
-    controls = merged[['subject_id', 'stay_id', 'referencetime']]
-    controls = controls.drop_duplicates(subset=["subject_id", "stay_id", "referencetime"])
-    
-    if int(len(controls)) > n:
-        controls = controls.sample(n=n, random_state=42)
-        return controls
-    
-    return controls
+diagnosis_path = os.path.join(hosp_dir, "diagnoses_icd.csv")
+#presenting illness
 
-def create_metadata_table(patient_list, icustays_df, patients_df, admissions_df, diagnosis_df, mapper):
-    '''Creates table of independent metadata features for each patient.'''
-    df = patient_list[['subject_id', 'stay_id']]
-    df = df.merge(icustays_df[['subject_id', 'stay_id', 'first_careunit']], on=['subject_id', 'stay_id'], how='left').rename(columns={'first_careunit': 'admission_type'})
-    df = df.merge(patients_df[['subject_id', 'gender', 'anchor_age']], on=['subject_id'], how='left').rename(columns={'anchor_age': 'age', 'gender': 'sex'})
-    
-    admissions_unique = admissions_df[['subject_id', 'race']].groupby('subject_id').first().reset_index()
-    df = df.merge(admissions_unique, on=['subject_id'], how='left')
-    stay_to_hadm = icustays_df.set_index('stay_id')['hadm_id']
-    df['hadm_id'] = df['stay_id'].map(stay_to_hadm)
+#TIME DEPENDENT
+chart_events_path = os.path.join(icu_dir, "chartevents.csv")
+vitals=['HR', 'RR', 'SpO2', 'SBP', 'DBP', 'MAP']
+#charttime, item_id mapping(temp: 223761, HR: 220045, RR: 220210, SpO2: 220277, SBP: 220179, DBP: 220180, Mean Arterial Pressure: 220181)
 
-    diagnosis_unique = diagnosis_df[diagnosis_df['seq_num'] == 1][['subject_id', 'icd_code', 'icd_version', 'seq_num', 'hadm_id']]
-    df = df.merge(diagnosis_unique, on=['subject_id', 'hadm_id'], how='left')
-    
-    icd9_mask = df['icd_version'] == 9
-    icd10_mask = df['icd_version'] == 10
+procedure_df = pd.read_csv(procedure_path)
+icustays_df = pd.read_csv(icustays_path)
+patients_df = pd.read_csv(patients_path)
+admissions_df = pd.read_csv(admissions_path)
+diagnosis_df = pd.read_csv(diagnosis_path)
 
-    mapped_icd10 = mapper.map(df.loc[icd9_mask, 'icd_code'], source="icd9", target="icd10")
+procedure_df["starttime"] = pd.to_datetime(procedure_df["starttime"])
+icustays_df["intime"] = pd.to_datetime(icustays_df["intime"])
+icustays_df["outtime"] = pd.to_datetime(icustays_df["outtime"])
 
-    mapped_icd10 = [x[0] if isinstance(x, list) else x for x in mapped_icd10]
-    
-    df.loc[icd9_mask, 'diagnosis'] = mapped_icd10
-    df.loc[icd10_mask, 'diagnosis'] = df.loc[icd10_mask, 'icd_code']
-    
-    return df[['subject_id', 'admission_type', 'sex', 'age', 'race', 'diagnosis']]    
 
-def query_vitals_vectorized(patient_df, vital, chart_events_path, hours_before=24):
-    '''Returns a polars df of a specific vital for hours_before hours before the reference time for all patients.'''
-    patient_df = patient_df.copy()
-    patient_df["referencetime"] = pd.to_datetime(patient_df["referencetime"])
-    patient_df["start_time"] = patient_df["referencetime"] - pd.Timedelta(hours=hours_before)
-    
-    patient_pl = pl.from_pandas(patient_df[["subject_id", "stay_id", "referencetime", "start_time"]]).lazy()
-    
-    vitals_lazy = (
-        pl.scan_csv(chart_events_path)
-        .with_columns(
-            pl.col("charttime").str.strptime(pl.Datetime, "%Y-%m-%d %H:%M:%S")
+ihca_list = preprocessing_query.create_ihca_list(procedure_df, icustays_df)
+controls_list = preprocessing_query.create_controls_list(icustays_df, ihca_list)
+mapper = Mapper()
+
+controls_metadata = preprocessing_query.create_metadata_table(controls_list, icustays_df, patients_df, admissions_df, diagnosis_df, mapper)
+#controls_metadata.to_csv("controls_metadata.csv", index=False)
+ihca_metadata = preprocessing_query.create_metadata_table(ihca_list, icustays_df, patients_df, admissions_df, diagnosis_df, mapper)
+#ihca_metadata.to_csv("ihca_metadata.csv", index=False)
+
+def create_vitals_table(input_list, label, hours_before=13):
+    #creates time dependent data tables
+    #label: classification of patient string "ctrl":0 vs "ihca":1
+    #input_list: ihca_list or controls_list above
+    #hours_before: how many hours before cardiac arrest event to start tracking
+    processed_vitals = {}
+    for vital_name in vitals:
+        vital_df = preprocessing_query.query_vitals_vectorized(input_list, vital_name, chart_events_path)
+        feature_df = preprocessing_query.feature_extract_vital(input_list, vital_df, vital_name, label, hours_before)
+        feature_df = (
+            feature_df
+            .unique(subset=["subject_id", "stay_id", "bin_hour"])  # Keep only unique rows
         )
-        .filter(pl.col("itemid") == vitals_code[vital])
-    )
-    
-    joined = vitals_lazy.join(
-        patient_pl,
-        on=["subject_id", "stay_id"],
-        how="inner"
-    )
-    
-    filtered = joined.filter(
-        (pl.col("charttime") >= pl.col("start_time")) &
-        (pl.col("charttime") <= pl.col("referencetime"))
-    ).select(["subject_id", "stay_id", "charttime", "valuenum"])
-    
-    df = filtered.collect()
-    return df
+        processed_vitals[vital_name] = feature_df
+    combined_df = processed_vitals["HR"].rename({"valuenum": "HR"})
+    for vital in vitals:
+        df = processed_vitals[vital].rename({"valuenum": vital})
+        # Only select the join keys and the vital column (not bin_time or other columns)
+        combined_df = combined_df.join(
+            df.select(["subject_id", "stay_id", "bin_hour", vital]),
+            on=["subject_id", "stay_id", "bin_hour"],
+            how="full",
+            coalesce=True
+        )
 
-def feature_extract_vital(patient_df, vitals_df, vital, condition, cutoff):
-    '''Applies last observation carried forward and feature extraction to queried vitals.'''
-    #patient_df(pandas df): patient list dataframe
-    #vitals_df(polars df): patient queried vitals dataframe
-    #vital(string): HR, RR, SpO2, MAP, DBP, SBP
-    #condition(string): ihca or ctrl
-    #cutoff(int): hours before reference event to include in final dataset
-
-    patient_df = patient_df.copy()
-    vitals_df = vitals_df.lazy()
-    patient_df["referencetime"] = pd.to_datetime(patient_df["referencetime"])
-
-    #create hourly bins for all reference times 24 hours before event
-    all_rows = []
-
-    for _, row in patient_df.iterrows():
-        ref_time = row["referencetime"]
-        subject_id = row["subject_id"]
-        stay_id = row["stay_id"]
-
-        for hour_subtract in range(0, 24):
-            bin_time = ref_time - pd.Timedelta(hours=hour_subtract)
-            all_rows.append({
-                "subject_id": subject_id,
-                "stay_id": stay_id,
-                "bin_hour": hour_subtract,
-                "bin_time":bin_time
-            })
-
-    bins = pl.from_pandas(pd.DataFrame(all_rows)).lazy()
-
-    joined = bins.join(vitals_df, on=["subject_id", "stay_id"], how="left")
-
-    #adds delta_hour column, helps identify rows created from cartesian expansion that do not fall into bin
-    joined = joined.with_columns([
-        ((pl.col("charttime") - pl.col("bin_time")).dt.total_seconds() / 3600).alias("delta_hour")
-    ])
-
-    #filters all datapoints that are outside of the bin due to cartesian expansion
-    joined = joined.filter(((pl.col("delta_hour") > -1) & (pl.col("delta_hour") <= 0)) | (pl.col("delta_hour").is_null()))
-
-    #aggregation methods to collapse per hour
-    if condition == "ctrl":  
-        agg_expr = get_agg_expr("valuenum", "mean")
-    elif condition == "ihca":
-        if vital in {"SBP", "DBP", "MAP", "SpO2"}:
-            agg_expr = get_agg_expr("valuenum", "min")
-        elif vital == "HR":
-            agg_expr = get_agg_expr("valuenum", conditional=True, threshold=80)
-        elif vital == "RR":
-            agg_expr = get_agg_expr("valuenum", conditional=True, threshold=12)
-        else:
-            raise ValueError("Invalid vital sign.")
-    else:
-        raise ValueError("Invalid condition group provided, must be ihca or ctrl.")
-    
-    aggregated = joined.group_by(["subject_id", "stay_id", "bin_hour", "bin_time"]).agg(agg_expr)
-    aggregated = aggregated.sort(["subject_id", "stay_id", "bin_hour"])
-
-    #rejoin hourly bin skeleton to keep all hourly timepoints for LOCF 
-    aggregated = bins.join(aggregated, on=["subject_id", "stay_id", "bin_hour", "bin_time"], how="left")
-
-    #Apply LOCF per patient
-    aggregated = aggregated.with_columns([
-        pl.col("valuenum").backward_fill().over(["subject_id", "stay_id"])
-    ])
-    
-    #filter only for "cutoff" hours before reference time
-    aggregated = aggregated.filter(pl.col("bin_hour") <= cutoff)
-
-    aggregated = aggregated.collect()
-    
-    return aggregated
-
-def get_agg_expr(col, agg_type=None, conditional=False, threshold=None):
-    expr = pl.col(col)
-
-    if conditional:
-        if threshold is None:
-            raise ValueError("Conditional aggregation requires a threshold value.")
-        else:
-            return (
-                # implements conditional feature extraction
-                pl.when(pl.min(col) >= threshold).then(pl.max(col)).otherwise(
-                    pl.when(pl.max(col) <= threshold).then(pl.min(col)).otherwise(pl.min(col)))
-                ) 
-    if agg_type == "mean":
-        return expr.mean()
-    elif agg_type == "min":
-        return expr.min()
-    else:
-        raise ValueError(f"Unknown aggregation type: {agg_type}")
-    
-def zscore(df, averages, std_devs):
-    if "date" in df.columns or "units" in df.columns:
-        columns_to_normalize = df.columns.difference(["date", "units"])
-    else:
-        columns_to_normalize = df.columns
-
-    global_avg = pd.Series(averages, index=columns_to_normalize)
-    global_std = pd.Series(std_devs, index=columns_to_normalize)
-    df[columns_to_normalize] = (df[columns_to_normalize] - global_avg) / global_std
-
-    return df, post_process_z_score(global_avg, global_std)
-
-def min_max_scaling(df, min, max):
-    # scale columns except date
-    if "date" in df.columns or "units" in df.columns:
-        columns_to_scale = df.columns.difference(["date", "units"])
-    else:
-        columns_to_scale = df.columns
-    columns_to_scale = df.columns.difference(['date', 'units'])
-    df[columns_to_scale] = (df[columns_to_scale] - min) / (max - min)
-    return df, post_process_min_max(min,max)
-
-def divide_by_100(df):
-    if "date" in df.columns or "units" in df.columns:
-        columns_to_divide = df.columns.difference(["date", "units"])
-    else:
-        columns_to_divide = df.columns
-    columns_to_divide = df.columns.difference(['date', 'units'])
-    df[columns_to_divide] = df[columns_to_divide] / 100
-    return df, post_process_divide_100()
+    combined_df = combined_df.select(["subject_id", "stay_id", "bin_hour", "bin_time", 'HR', 'RR', 'SpO2', 'SBP', 'DBP', 'MAP']).sort("subject_id")
+    for vital in vitals:
+        combined_df = preprocessing_query.impute_patient_average_column(combined_df, vital)
+    combined_df = combined_df.with_columns(pl.lit(1 if label == "ihca" else 0).alias("label")) 
+    return combined_df
 
 
-def one_hot_encode():
-    pass
+
+ihca_output = create_vitals_table(ihca_list,"ihca")
+ctrl_output = create_vitals_table(controls_list,"ctrl")
+combined_df = pl.concat(
+    [ihca_output, ctrl_output]
+)
+
+metadata = pl.from_pandas(pd.concat([controls_metadata, ihca_metadata]))
+final_df = combined_df.join(metadata, how="left", on="subject_id")
+
+final_df.write_csv("output.csv")
